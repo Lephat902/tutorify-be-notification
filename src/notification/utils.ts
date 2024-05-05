b595348569246f7b3278b4c7c76ce20d002f0d32
Lephat902 <lehoangngocphat992002@gmail.com>
2024-05-05 18:12:53 +0700

    Fix redundant space in user full name build

--- src/notification/utils.ts
+++ src/notification/utils.ts
@@ -2,6 +2,8 @@ import { BasicUserInfoDto } from "./proxies/dtos";
 
 export class Utils {
     static getFullName(user: BasicUserInfoDto) {
-        return `${user.firstName} ${user.middleName} ${user.lastName}`;
+        return [user.firstName, user.middleName, user.lastName]
+            .filter(name => name) // This will remove any undefined or empty strings
+            .join(' '); // This will join the names with a space
     }
 }
